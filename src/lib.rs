5a2723c42d1856d6c23d696b6099427ec8146c48
Lúcás Meier <cronokirby@gmail.com>
2019-06-04 17:26:45 +0200

    Don't scale values incorrectly

--- src/lib.rs
+++ src/lib.rs
@@ -71,8 +71,7 @@ fn spawn_audio_loop(rx: Receiver<f32>) -> (u32, thread::JoinHandle<()>) {
                     for sample in output.chunks_mut(channels) {
                         let value = rx.recv().unwrap();
                         for out in sample.iter_mut() {
-                            // convert between 0,1 and -1 1
-                            *out = 2.0 * value - 1.0;
+                            *out = value;
                         }
                     }
                 }
